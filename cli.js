d06ecf2edc6d6066c0342e5c65effe004d2450d5
Elizabeth Vega M <eb.vega.m@gmail.com>
2018-08-23 20:28:08 -0300

    WIP replanteando

--- cli.js
+++ cli.js
@@ -2,11 +2,10 @@
 
 const fs = require('fs');
 const path = require('path');
-const [, , ...args] = process.argv;
 const index = require('../scl-2018-01-FE-markdown/index');
 
 // Lee los archivos que se encuentran en la carpeta seleccionada
-fs.readdir(`${args}`, function(err, files) {
+fs.readdir(process.argv[2], 'utf8', function(err, files) {
   if (err) throw err;
   console.log(files);
   for (let i = 0; i < files.length; i++) {
