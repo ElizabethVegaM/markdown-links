2c1ada9908fa31a9fba8de5e9da991fa85e835b1
Elizabeth Vega M <eb.vega.m@gmail.com>
2018-08-31 21:29:10 -0300

    añadi colores al output

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mdLinks = require('../scl-2018-01-FE-markdown/mdlinks').mdLinks;
 const path = require('path');
 const fetch = require('node-fetch');
 const [, , ...args] = process.argv;
-var colors = require('colors/safe');
+const colors = require('colors');
 // en el caso de arriba omite las primeras dos palabras que escribe el usuario en la consola
 // por cada espacio se hace un nuevo elemento en process.argv
 
@@ -13,16 +13,16 @@ if (require.main === module) {
   if (process.argv.includes('--validate')) options.validate = true;
   if (process.argv.includes('--stats')) options.stats = true;
   mdLinks(path.join(process.cwd(), args[0]), options).then((links) => {
-    let result = [];
+    let result = '';
     let successCounter = 0;
     let failCounter = 0;
     links.map(element => {
       fetch(element.href)
         .then(res => {
           if (options.validate) {
-            result.push(`${element.file}: ${element.line} - ${element.href} ${element.text} ${res.status} ${res.ok}`);
+            result = (`${element.file}: ${element.line} - ${colors.magenta(element.href)} - ${element.text} ${colors.green(res.status)} ${colors.green(res.ok)}`);
           } else {
-            result.push(`${element.file} : ${element.line} - ${element.href} ${element.text}`);
+            result = (`${colors.grey(element.file)}: ${colors.grey(element.line)} - ${colors.cyan(element.href)} - ${element.text}`);
           }
           if (options.stats) {
             if (res.ok === true) {
@@ -30,13 +30,11 @@ if (require.main === module) {
             } else if (res.ok === false) {
               failCounter++;
             }
-            result.push({
-              totals: links.filter(link => link.href).length,
-              success: colors.green(successCounter),
-              failure: failCounter
-            });
+            stats = (`${colors.cyan('totals: ')} ${colors.cyan(links.filter(link => link.href).length)}, ${colors.green('success: ')} ${colors.green(successCounter)}, ${colors.red('failed: ')} ${colors.red(failCounter)}`);
           }
           console.log(result);
+          if (options.stats) console.log(stats);
+          console.log(colors.green('hello'));
         })
         .catch(err => {
           console.error(err);
